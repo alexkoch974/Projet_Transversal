4ece541cd8f0b625b1b6f3b975b4f7e238fa4ff3
Alexandre Koch <alexandrekoch3@gmail.com>
2022-11-29 21:53:28 +0100

    Sorted the points on my head for avatar matching

--- Test_cam.py
+++ Test_cam.py
@@ -4,7 +4,16 @@ import mediapipe as mp
 
 mp_face_mesh = mp.solutions.face_mesh
 
-FACE = [67, 103, 54, 297, 332, 284, 10, 152, 234, 454, 473, 474, 475, 476, 477, 468, 469, 470, 471, 472, 173, 159, 33, 145, 263, 386, 398, 374, 308, 13, 78, 14]
+EAR_L = [103, 67, 109]
+EAR_R = [338, 297, 332]
+FACE = [152, 234, 10, 454]
+EYE_L = [145, 476, 159, 474]
+EYE_R = [374, 471, 386, 469]
+MOUSTACHE_L = [214, 187]
+MOUSTACHE_R = [434, 411]
+MOUTH = [14, 78, 13, 308]
+
+ALL_POINTS = EAR_L + EAR_R + FACE + EYE_L + EYE_R + MOUSTACHE_L + MOUSTACHE_R + MOUTH
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
@@ -28,10 +37,11 @@ with mp_face_mesh.FaceMesh(
     results = face_mesh.process(image)
     
     if results.multi_face_landmarks:
-      points_mat = np.array([[int(results.multi_face_landmarks[0].landmark[i].x*image.shape[1]), int(results.multi_face_landmarks[0].landmark[i].y*image.shape[0])] for i in FACE])
+      points_mat = np.array([[int(results.multi_face_landmarks[0].landmark[i].x*image.shape[1]), int(results.multi_face_landmarks[0].landmark[i].y*image.shape[0])] for i in ALL_POINTS])
       for point in points_mat:
         cv2.circle(image, (int(point[0]), int(point[1])), 1, (0, 255, 0), 1)
     cv2.imshow('Test', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
+    key = cv2.waitKey(1)
+    if key == ord('q'):
       break
 cap.release()
